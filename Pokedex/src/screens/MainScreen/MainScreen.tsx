4e93252b4abbbd8d10fa40d154c57926f8326a41
Gabriel Willans <g.willans@outlook.com>
2021-09-28 11:04:38 -0300

    Svg files setup

--- Pokedex/src/screens/MainScreen/MainScreen.tsx
+++ Pokedex/src/screens/MainScreen/MainScreen.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, TextInput, View } from 'react-native'
+import Pokeball from '../../../assets/patterns/pokeball-grad.svg';
 
 const MainScreen = () => {
 
     return (
-        <View>
-            <View style={style.view2}></View>
+        <View style={style.container}>
+            <View style={style.pokeball}>
+                <Pokeball height={500} width={500} />
+            </View>
             <View style={style.contentContainer}>
                 <Text style={style.title} >Pokédex</Text>
                 <Text style={style.subTitle}>Search for Pokémon by name or using the National Pokédex number.</Text>
@@ -18,28 +21,36 @@ const MainScreen = () => {
 }
 
 const style = StyleSheet.create({
-    contentContainer: {
-        padding: 8
+    container:{
+        backgroundColor: 'white',
+        height: '100%',
+        width: '100%',
     },
-    view2:{
-        backgroundColor: 'red',
-        height: 50,
+    contentContainer: {
+        padding: 24
+    },
+    pokeball: {
         width: '100%',
         position: 'absolute',
-        top: 0
+        top: -250,
+        alignItems: 'center'
     },
     title: {
         fontWeight: 'bold',
         color: 'black',
-        fontSize: 34
+        fontSize: 34,
+        margin: 8,
+        marginTop:32
     },
     subTitle: {
         fontSize: 20,
-        color: '#555'
+        color: '#666',
+        margin: 8
     },
     textInputContainer: {
         backgroundColor: '#eee',
-        borderRadius: 8
+        borderRadius: 8,
+        margin: 8
     }
 });
 
